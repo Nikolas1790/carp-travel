dfa7921c4514221c2f834cf0b6d6b393cf5dd3ff
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-30 23:39:16 +0300

    services-img

--- src/app/components/servicesContentImgs.tsx
+++ src/app/components/servicesContentImgs.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ContentImg } from '../../lib/utils/utils';
 import { FC } from 'react';
 
@@ -7,27 +8,17 @@ interface ServicesContentImgsProps {
 }
 
 const ServicesContentImgs: FC<ServicesContentImgsProps> = ({ title, contentImg }) => {
-  return (
-    <picture className="md:col-start-1 md:row-start-2 ">
-      <source
-        srcSet={`${contentImg['sm-mob']} 1x, ${contentImg['sm-mob-2x']} 2x`}
-        media="(max-width: 767px)"
-      />
-      <source
-        srcSet={`${contentImg['md-tab']} 1x, ${contentImg['md-tab-2x']} 2x`}
-        media="(max-width: 1280px)"
-      />
-      <source
-        srcSet={`${contentImg['lg-desktop']} 1x, ${contentImg['lg-desktop-2x']} 2x`}
-        media="(min-width: 1280px)"
-      />
-      <img
-      className="max-w-full h-auto mb-3 md:mb-0"
-        src={contentImg['lg-desktop']}
-        alt={title}                    
-        loading='lazy'  
-      />
-    </picture>
+  return (    
+    <Image
+      className="h-[213px] max-w-full mb-3 md:col-start-1 md:row-start-2 md:h-[370px]  md:mb-0 xl:h-[429px] "
+      src={contentImg['lg-desktop']}
+      alt={title}
+      objectFit="cover"
+      width={607}
+      height={429}
+      sizes="(max-width: 767px) 100vw, (max-width: 1280px) 100vw, 100vw"
+      loading="lazy"
+    />
   );
 };
 
