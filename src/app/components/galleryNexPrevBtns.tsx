b34ea1aececdb08bef7d2c078bf4592027f0e1eb
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-31 00:55:56 +0300

    gallery-dell-mob-btns

--- src/app/components/galleryNexPrevBtns.tsx
+++ src/app/components/galleryNexPrevBtns.tsx
@@ -14,13 +14,13 @@ const GalleryNextPrevBtns: FC<GalleryNextPrevBtnsProps> = ({  gallery }) => {
     <>
       <button
         type="button"
-        className="prev-button md:absolute md:z-10 md:bottom-[90px] md:left-[50px] md:text-[33px] md:font-thin  xl:bottom-[90px] xl:left-[228px] hover:scale-105 focus:scale-107 transition" 
+        className=" prev-button hidden md:block md:absolute md:z-10 md:bottom-[90px] md:left-[50px] md:text-[33px] md:font-thin  xl:bottom-[90px] xl:left-[228px] hover:scale-105 focus:scale-107 transition" 
       >
         {gallery.btnBack}
       </button>
       <button
         type="button"
-        className="next-button md:absolute md:z-10 md:bottom-[90px] md:right-[50px] md:text-[33px] md:font-thin  xl:bottom-[90px] xl:right-[228px] hover:scale-105 focus:scale-107 transition"                  
+        className="next-button hidden md:block md:absolute md:z-10 md:bottom-[90px] md:right-[50px] md:text-[33px] md:font-thin  xl:bottom-[90px] xl:right-[228px] hover:scale-105 focus:scale-107 transition"                  
       >
         {gallery.btnNext}
       </button>     
